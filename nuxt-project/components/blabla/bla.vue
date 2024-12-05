f1edac8b4fe0780d292ea1be326a9c47bce3abeb
nguyenvietbinh <126882221+nguyenvietbinh@users.noreply.github.com>
2024-12-05 01:44:34 +0000

    blabal2

---
+++ nuxt-project/components/blabla/bla.vue
@@ -0,0 +1,13 @@
+<script setup>
+async function submit() {
+  const res = await $fetch('/api/submit', {
+    method: 'post',
+    body: JSON.stringify({ message: 'hello' })
+  })
+  console.log(res);
+}
+</script>
+
+<template>
+  <button @click="submit">blabla</button>
+</template>
---
+++ nuxt-project/pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+
+    <BlablaBla></BlablaBla>
+</template>
